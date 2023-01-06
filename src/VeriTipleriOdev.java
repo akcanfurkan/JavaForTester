2115cde552e059828d930f528227a74f29321c6f
Furkan Akcan <akcanfurkan@hotmail.com.tr>
2023-01-06 21:54:12 +0300

    Initialized files

---
+++ src/VeriTipleriOdev.java
@@ -0,0 +1,14 @@
+public class VeriTipleriOdev {
+
+    public static void main(String[] args) {
+
+    String[] programlamaDilleri = {"Java", "C", "Python"};
+    int[] seviye = {1, 2, 3,4,5};
+
+    System.out.println("Progralama dili: " + programlamaDilleri[0] + " Seviye: " + seviye[3]);
+    System.out.println("Progralama dili: " + programlamaDilleri[1] + " Seviye: " + seviye[0]);
+    System.out.println("Progralama dili: " + programlamaDilleri[2] + " Seviye: " + seviye[1]);
+    
+}
+
+}

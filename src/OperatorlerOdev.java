bd5d6255a5746069fdf538b31aa3041904b0bbdd
Furkan Akcan <akcanfurkan@hotmail.com.tr>
2023-01-07 00:12:53 +0300

    new files added

---
+++ src/OperatorlerOdev.java
@@ -0,0 +1,18 @@
+public class OperatorlerOdev {
+    public static void main(String[] args) {
+
+        int a = 85;
+        int b = 45;
+        int toplam = a + b;
+        int carpim = a * b;
+
+        System.out.println(toplam);
+        System.out.println(carpim);
+
+        if (carpim > 3000 && carpim < 4000) {
+            System.out.println(carpim);
+
+
+    }
+}
+    }
